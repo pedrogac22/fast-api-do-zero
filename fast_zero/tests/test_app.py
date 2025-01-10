8384ce4ffccae5b9175ce336e2a1edd9b29bc579
Pedro Gomes <pedrogac2@gmail.com>
2025-01-10 18:27:50 -0300

    Configuração inicial do projeto

---
+++ fast_zero/tests/test_app.py
@@ -0,0 +1,20 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+client = TestClient(app)
+
+
+def test_root_deve_retornar_ok_e_ola_mundo():
+    
+    # Arrange
+    client = TestClient(app) 
+
+    # Act
+    response = client.get('/')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
