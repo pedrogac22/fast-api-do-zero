6b7f348a84fbb19d8a472304486b0a7a3a87f988
Pedro Gomes <pedrogac2@gmail.com>
2025-01-12 20:34:02 -0300

    adicionando testes de exceptions

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -56,8 +56,29 @@ def test_update_user(client):
     }
 
 
+def test_update_user_with_exception(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user(client):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_with_exception(client):
+    response = client.delete('/users/0')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
